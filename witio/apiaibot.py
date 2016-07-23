5338348dfc10fedeac9ad05dbc1c397a77f97e35
Akshat <akshat54@gmail.com>
2016-07-23 14:10:37 +0530

    debug

--- witio/apiaibot.py
+++ witio/apiaibot.py
@@ -35,7 +35,7 @@ class MyApiaiClient(apiai.ApiAI):
         action = result.get('action')
         actionIncomplete = result.get('actionIncomplete', False)
         message = response['result']['fulfillment']['speech']
-        print "message - " + str(message)
+        print "message - " + str(message) + "type=" + str(type(message))
 
         if action is not None:
             if not actionIncomplete:
